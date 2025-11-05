7092968855915ca7624cf3c0d910ca4224c35442
Chetan-Frameboxx <chetan.frameboxxers@gmail.com>
2025-11-05 16:59:07 +0530

    Lecture data

--- React/firstapp/src/App.jsx
+++ React/firstapp/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css"
 import Navbar from './Pages/Navbar'
 import Footer from './Pages/Footer'
 import Button from './Pages/Button'
+import Home from './Pages/Home'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <h1>Namaste Bharat</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam laudantium quia obcaecati accusantium saepe facere! Ratione, ipsa voluptates! Optio enim non doloremque officiis, veniam quibusdam iure officia vel quia temporibus.</p>
       <Button/>
+      <Home/>
       <Footer/>
       
     </div>
---
+++ React/firstapp/src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+function Home() {
+  return (
+    <div>
+      <Navbar />
+         HOME
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
